74e610dd2423ecb457d766e4c6b0d510deb2e5df
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-04-20 21:57:05 +0900

    Start reading Ch.5

---
+++ src/main/scala/yokohama/Ch5.scala
@@ -0,0 +1,6 @@
+package yokohama.murataku
+object Ch5 extends App {
+  trait Stream[+A]
+  case object Empty extends Stream[Nothing]
+  case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+}
