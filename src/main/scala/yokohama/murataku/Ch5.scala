60b5ca3477b0820c9c9396f1bfb63e9642804434
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2017-04-21 09:51:26 +0900

    Add smart constructor

--- src/main/scala/yokohama/murataku/Ch5.scala
+++ src/main/scala/yokohama/murataku/Ch5.scala
@@ -3,4 +3,14 @@ object Ch5 extends App {
   trait Stream[+A]
   case object Empty extends Stream[Nothing]
   case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+  object Stream {
+    def cons[A](head: => A, tail: => Stream[A]): Stream[A] = {
+      lazy val h = head
+      lazy val t = tail
+      Cons(() => h, () => t);
+    }
+
+    def empty[A]: Stream[A] = Empty
+  }
 }
